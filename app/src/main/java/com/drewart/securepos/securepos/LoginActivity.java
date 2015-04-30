1ceb811edf0fea5efabf7d8f17680a035c806a0b
unknown <chew.andreas@gmail.com>
2015-04-30 08:12:21 -0700

    Added code to setup activity to add user to db Added permision to use internet connectivity

--- app/src/main/java/com/drewart/securepos/securepos/LoginActivity.java
+++ app/src/main/java/com/drewart/securepos/securepos/LoginActivity.java
@@ -4,12 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -32,14 +27,14 @@ import java.util.List;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends Activity  {
 
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world, test@test:test1"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -59,7 +54,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -93,9 +87,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         mProgressView = findViewById(R.id.login_progress);
     }
 
-    private void populateAutoComplete() {
-        getLoaderManager().initLoader(0, null, this);
-    }
+
 
 
     public void signUp() {
@@ -200,40 +192,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<String>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
     }
 
     private interface ProfileQuery {
--- app/src/main/java/com/drewart/securepos/securepos/MainActivity.java
+++ app/src/main/java/com/drewart/securepos/securepos/MainActivity.java
@@ -1,25 +1,64 @@
 package com.drewart.securepos.securepos;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private AddUser mAuthTask = null;
+    private String name;
+    private String email;
+    private String phone;
+    private String bank;
+    private InputStream is=null;
+    private String result=null;
+    private String line=null;
+    private int code;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       final EditText e_name    = (EditText) findViewById(R.id.editTextName);
+       final EditText e_email   = (EditText) findViewById(R.id.editTextEmail);
+       final EditText e_phone   = (EditText) findViewById(R.id.editTextPhone);
+       //final EditText e_bank    = (EditText) findViewById(R.id.bankOption);
+
         Button mSignUpButton = (Button)findViewById(R.id.signUp_button);
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                name    = e_name.getText().toString();
+                email   = e_email.getText().toString();
+                phone   = e_phone.getText().toString();
+                //bank    = e_bank.getText().toString();
                 signUp();
             }
         });
@@ -50,8 +89,108 @@ public class MainActivity extends ActionBarActivity {
 
 
     public void signUp() {
-        Intent i = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(i);
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("name",name));
+        nameValuePairs.add(new BasicNameValuePair("email",email));
+        nameValuePairs.add(new BasicNameValuePair("phone",phone));
+        //nameValuePairs.add(new BasicNameValuePair("bank",bank));
+
+        mAuthTask = new AddUser(nameValuePairs);
+    }
+
+    public class AddUser extends AsyncTask<Void, Void, Boolean> {
+
+        private final ArrayList<NameValuePair> nameValuePairs;
+
+        AddUser (ArrayList<NameValuePair> nvp) {
+            nameValuePairs = nvp;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            try
+            {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("https://students.washington.edu/andreas5/insert.php");
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+                Log.e("pass 1", "connection success ");
+            }
+            catch(Exception e)
+            {
+                Log.e("Fail 1", e.toString());
+                Toast.makeText(getApplicationContext(), "Invalid IP Address",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            try
+            {
+                BufferedReader reader = new BufferedReader
+                        (new InputStreamReader(is,"iso-8859-1"),8);
+                StringBuilder sb = new StringBuilder();
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result = sb.toString();
+                Log.e("pass 2", "connection success ");
+            }
+            catch(Exception e)
+            {
+                Log.e("Fail 2", e.toString());
+            }
+
+            try
+            {
+                JSONObject json_data = new JSONObject(result);
+                code=(json_data.getInt("code"));
+
+                if(code==1)
+                {
+                    Toast.makeText(getBaseContext(), "Inserted Successfully",
+                            Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "Sorry, Try Again",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+            catch(Exception e)
+            {
+                Log.e("Fail 3", e.toString());
+            }
+
+            return true;
+        }
+
+//        @Override
+//        protected void onPostExecute(final Boolean success) {
+//            mAuthTask = null;
+//            showProgress(false);
+//
+//            if (success) {
+//                finish();
+//            } else {
+//                mPasswordView.setError(getString(R.string.error_incorrect_password));
+//                mPasswordView.requestFocus();
+//            }
+//        }
+//
+//        @Override
+//        protected void onCancelled() {
+//            mAuthTask = null;
+//            showProgress(false);
+//        }
+
     }
 }
 
