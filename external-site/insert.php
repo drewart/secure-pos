e8ae3db09581d99d72a426d522c0cadae84d79b4
PieDr001 <Drew.Pierce@staples.com>
2015-05-03 23:03:14 -0700

    adding external website interface insert.php

---
+++ external-site/insert.php
@@ -0,0 +1,27 @@
+
+<?php
+	$host='vergil.u.washington.edu:39865';
+	$uname='root';
+	$pwd='password';
+	$db="android";
+
+	$con = mysql_connect($host,$uname,$pwd) or die("connection failed");
+	mysql_select_db($db,$con) or die("db selection failed");
+	 
+	
+	$name=$_REQUEST['name'];
+	$email=$_REQUEST['email'];
+	$phone=$_REQUEST['phone'];
+	
+
+	$flag['code']=0;
+
+	if($r=mysql_query("insert into users values('$name', '$email', '$phone', NULL) ",$con))
+	{
+		$flag['code']=1;
+		echo"hi";
+	}
+
+	print(json_encode($flag));
+	mysql_close($con);
+?>
